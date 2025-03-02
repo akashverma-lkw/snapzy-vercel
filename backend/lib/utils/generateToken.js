1a7e6f48f2d2a72a39e569f89b1c6242649656db
Akash Verma <akashvermastp7@gmail.com>
2025-03-02 14:36:14 +0530

    update 3 files

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -6,10 +6,9 @@ export const generateTokenAndSetCookie = (userId, res) => {
 	});
 
 	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000,
-		httpOnly: true,
-		sameSite: "none",
+		maxAge: 15 * 24 * 60 * 60 * 1000, //MS
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		sameSite: "none", // CSRF attacks cross-site request forgery attacks
 		secure: process.env.NODE_ENV === "production",
 	});
-	res.setHeader("Access-Control-Allow-Credentials", "true"); // 🔥 Must for Vercel
 };
